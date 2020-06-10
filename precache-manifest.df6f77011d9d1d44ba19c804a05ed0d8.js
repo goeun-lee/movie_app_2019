ff1305d27c3a86d354881c8ce0845bb731166fea
goeun-lee <goeun0104@naver.com>
2020-06-10 21:02:29 +0900

    Updates

---
+++ precache-manifest.df6f77011d9d1d44ba19c804a05ed0d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a40f1b4f24bc2e95b5be11cbef526077",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "2535c27ed47dea15080c",
+    "url": "/movie_app_2019/static/css/main.19c21050.chunk.css"
+  },
+  {
+    "revision": "7676ec8dfa3a757764b3",
+    "url": "/movie_app_2019/static/js/2.cba50508.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2019/static/js/2.cba50508.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2535c27ed47dea15080c",
+    "url": "/movie_app_2019/static/js/main.f2883ff6.chunk.js"
+  },
+  {
+    "revision": "f5b00c7a31981ed629ee",
+    "url": "/movie_app_2019/static/js/runtime-main.140b3552.js"
+  }
+]);
